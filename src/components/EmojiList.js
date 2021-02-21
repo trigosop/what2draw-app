9e750308bc9aeef353fdab75f8365e6bcd09f64b
Piero Trigoso <trigosop@gmail.com>
2021-02-21 03:36:15 -0300

    ajustes varios

--- src/components/EmojiList.js
+++ src/components/EmojiList.js
@@ -3,13 +3,13 @@ import Emojis from '../data/emojis.json';
 export default function EmojiList() {
   return (
     <div className="Emojis">
-      <span>
+      <span className="Emoji">
         {Emojis[Math.floor(Math.random() * Math.floor(Emojis.length))]}
       </span>
-      <span>
+      <span className="Emoji">
         {Emojis[Math.floor(Math.random() * Math.floor(Emojis.length))]}
       </span>
-      <span>
+      <span className="Emoji">
         {Emojis[Math.floor(Math.random() * Math.floor(Emojis.length))]}
       </span>
     </div>
